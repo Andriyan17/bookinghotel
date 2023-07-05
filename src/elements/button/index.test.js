45263a1ee09c6e86934e1c6d680a01211a5711d7
andriyan ginting <andrginting17@gmail.com>
2023-07-05 23:57:53 +0700

    Update `index.test.js`

--- src/elements/button/index.test.js
+++ src/elements/button/index.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+/* eslint-disable jest/valid-expect */
 /* eslint-disable testing-library/no-node-access */
 /* eslint-disable testing-library/no-container */
 import React from 'react';
@@ -8,4 +10,12 @@ test('Should not allowed click button if isDisable is present', () => {
   const { container } = render(<Button isDisabled></Button>);
 
   expect(container.querySelector('span.disabled')).toBeInTheDocument();
+});
+
+test('Should render loading/spinner', () => {
+  const { container, getByText } = render(<Button isLoading></Button>);
+
+  expect(getByText(/loading/i)).toBeInTheDocument();
+
+  expect(container.querySelector('span')).toBeInTheDocument();
 });
