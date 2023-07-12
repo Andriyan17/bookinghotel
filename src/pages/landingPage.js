c0fe89dac3f22e5bf9478f154e83a2b61e683761
andriyan ginting <andrginting17@gmail.com>
2023-07-12 16:02:21 +0700

    Create grid for section mostpicked

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from 'parts/Header';
 import Hero from 'parts/Hero';
 import landingPage from 'json/landingPage.json';
-import mostPicked from 'parts/MostPicked';
+import MostPicked from 'parts/MostPicked';
 
 export default class LandingPage extends Component {
   render() {
@@ -10,7 +10,7 @@ export default class LandingPage extends Component {
       <>
         <Header {...this.props} location={window.location}></Header>
         <Hero data={landingPage.hero} />
-        <mostPicked data={landingPage.mostPicked} />
+        <MostPicked data={landingPage.mostPicked} />
       </>
     );
   }
--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
 export default function MostPicked(props) {
-  return <section className="container"></section>;
+  return (
+    <section className="container">
+      <h4 className="mb-3">MostPicked</h4>
+      <div className="container-grid">
+        <div className="item column-4 row-2">
+          <div className="card">Card 1</div>
+        </div>
+        <div className="item column-4 row-1">
+          <div className="card">card 2</div>
+        </div>
+        <div className="item column-4 row-1">
+          <div className="card">card 3</div>
+        </div>
+        <div className="item column-4 row-1">
+          <div className="card">card 4</div>
+        </div>
+        <div className="item column-4 row-1">
+          <div className="card">card 5</div>
+        </div>
+      </div>
+    </section>
+  );
 }
