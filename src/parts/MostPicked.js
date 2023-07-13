73689ec3a20d6d5f43cb794930d1267634acc279
andriyan ginting <andrginting17@gmail.com>
2023-07-13 21:47:30 +0700

    Create markup mostpicked at `mostPicked.js`

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,3 +1,4 @@
+import Button from 'elements/Button';
 import React from 'react';
 
 export default function MostPicked(props) {
@@ -5,21 +6,37 @@ export default function MostPicked(props) {
     <section className="container">
       <h4 className="mb-3">MostPicked</h4>
       <div className="container-grid">
-        <div className="item column-4 row-2">
-          <div className="card">Card 1</div>
-        </div>
-        <div className="item column-4 row-1">
-          <div className="card">card 2</div>
-        </div>
-        <div className="item column-4 row-1">
-          <div className="card">card 3</div>
-        </div>
-        <div className="item column-4 row-1">
-          <div className="card">card 4</div>
-        </div>
-        <div className="item column-4 row-1">
-          <div className="card">card 5</div>
-        </div>
+        {props.data.map((item, index) => {
+          return (
+            <div
+              className={`item column-4${index === 0 ? ' row-2' : ' row-1'}`}>
+              <div className="card card-feature">
+                <div className="tag">
+                  ${item.price}
+                  <span className="font-weight-light">per {item.unit}</span>
+                </div>
+                <figure className="img-wrapper">
+                  <img
+                    src={item.imageUrl}
+                    alt={item.name}
+                    className="img-cover"
+                  />
+                </figure>
+                <div className="meta-wrapper">
+                  <Button
+                    type="link"
+                    class="streched-link d-block text-white"
+                    href={`/properties/${item._id}`}>
+                    <h5>{item.name}</h5>
+                  </Button>
+                  <span>
+                    {item.city}, {item.country}
+                  </span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
