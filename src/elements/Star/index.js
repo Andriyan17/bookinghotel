42918cce1376e1945bae9eb7db29a10c527f54cf
andriyan ginting <andrginting17@gmail.com>
2023-07-16 22:38:09 +0700

    Create function for star at  `index.js`

---
+++ src/elements/Star/index.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import propTypes from 'prop-types';
+
+export default function Star({ value, height, width, spacing }) {
+  const decimals = Number(value) % 1;
+  const star = [];
+  let letfPos = 0;
+  for (let index = 0; index < 5 && index < value - decimals; index++) {
+    letfPos = letfPos * width;
+    star.push(
+      <div
+        className="star"
+        key={`star-${index}`}
+        style={{
+          left: index * width,
+          width: width,
+          marginRight: spacing,
+        }}></div>
+    );
+  }
+  if (decimals > 0 && value <= 5)
+    star.push(
+      <div
+        className="star"
+        key={`star-${index}`}
+        style={{
+          left: letfPos,
+          width: width - spacing,
+        }}></div>
+    );
+
+  const starPlaceHolder 
+
+  return <div>Star</div>;
+}
+
+Star.propTypes = {
+  className: propTypes.string,
+  value: propTypes.number,
+  width: propTypes.number,
+  spacing: propTypes.number,
+  height: propTypes.number,
+};
