96a9e8e66fc427a445914116ea360f7d4dee3a60
andriyan ginting <andrginting17@gmail.com>
2023-07-19 21:20:02 +0700

    Update `Footer.js`

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -1,5 +1,84 @@
+import Button from 'elements/Button';
 import React from 'react';
+import IconText from './iconText';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <IconText />
+            <p className="brand-tag-line">
+              we kabbom your beauty holiday instantly and memorable
+            </p>
+          </div>
+          <div className="col-2 mr-5">
+            <h6 className="mt-2">For Beginners</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="/register">
+                  New Account
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/properties">
+                  Start Booking Room
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/use-payment">
+                  Use Payments
+                </Button>
+              </li>
+            </ul>
+          </div>
+          <div className="col-2 mr-5">
+            <h6 className="mt-2">Explore Us</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button type="link" href="/careers">
+                  Our Careers
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/privacy">
+                  Privacy
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button type="link" href="/terms">
+                  Term & Conditions
+                </Button>
+              </li>
+            </ul>
+          </div>
+          <div className="col-4">
+            <h6 className="mt-2">Contact Us</h6>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <Button
+                  isExternal
+                  type="link"
+                  href="mailto:andrginting@gmail.com">
+                  Our Careers
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <Button isExternal type="link" href="tel:+622122081996">
+                  2122081996
+                </Button>
+              </li>
+              <li className="list-group-item">
+                <span>Staycation, Kemang, Jakarta</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text-center copyright">copyright 2023</div>
+        </div>
+      </div>
+    </footer>
+  );
 }
