067864a799b39d93a5d50145b612cba8c11ee4bd
andriyan ginting <andrginting17@gmail.com>
2023-07-23 18:49:49 +0700

    Create Onchange function in `Number Elements`

--- src/elements/Form/InputDate/index.js
+++ src/elements/Form/InputDate/index.js
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import propTypes from 'prop-types';
+import './index.scss';
 
-export default function index() {
-  return <div>index</div>;
+export default function Number(props) {
+  const { value, placeholder, name, min, max, prefix, suffix } = props;
+
+  const { InputValue, setInputValue } = useState(`${prefix}${value}${suffix}`);
+
+  const onChange = (e) => {
+    let value = String(e.target.value);
+    if (prefix) value = value.replace(prefix);
+    if (suffix) value = value.replace(suffix);
+
+    const patternNumeric = new RegExp('[0-9]*');
+    const isNumeric = patternNumeric.test(value);
+
+    if (isNumeric && +value <= max && +value >= min) {
+      props.onChange({
+        target: {
+          name: name,
+          value: +value,
+        },
+      });
+      setInputValue(`${prefix}${value}${suffix}`);
+    }
+  };
+
+  return <div></div>;
 }
+
+Number.defaultProps = {
+  min: 1,
+  max: 1,
+  prefix: '',
+  suffix: '',
+};
+
+Number.propTypes = {
+  value: propTypes.oneOfType([propTypes.string, propTypes.number]),
+  onChange: propTypes.func,
+  placeholder: propTypes.string,
+  outerClassName: propTypes.string,
+};
