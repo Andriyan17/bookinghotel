a9d4903200a89a2c28d4e17c9b98d5cecc5cf365
andriyan ginting <andrginting17@gmail.com>
2023-07-26 18:25:46 +0700

    Update `index.js` in BreadCrumbs

--- src/elements/BreadCrumb/index.js
+++ src/elements/BreadCrumb/index.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import propTypes from 'prop-types'
+import './index.scss'
+import Button from 'elements/Button';
 
-export default function index() {
+export default function BreadCrumb(props) {
+  const className = ['breadcrumb', props.className];
   return (
-    <div>index</div>
-  )
+    <nav aria-label="breadcrumb">
+      <ol className={className.join(' ')}>
+        {props.data.map((item, index) => {
+          return (
+            <li
+              key={`breadcrumb-${index}`}
+              className={`breadcrumb-item${
+                index === props.data.length - 1 ? ' active' : ''
+              }`}>
+              {index === props.data.length - 1 ? (
+                item.pageTitle
+              ) : (
+                <Button type="link" href={item.pageHref}>
+                  {item.pageTitle}
+                </Button>
+              )}
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
+  );
+}
+
+BreadCrumb.propTypes = {
+    data: propTypes.array,
+    className: propTypes.string
 }
