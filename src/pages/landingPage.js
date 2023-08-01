5a8cd19399853d81dbc29439804b4ea17ea365d1
andriyan ginting <andrginting17@gmail.com>
2023-08-01 21:28:44 +0700

    Update `LandingPage.js`

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -12,6 +12,12 @@ export default class LandingPage extends Component {
     super(props);
     this.refMostPicked = React.createRef();
   }
+
+  componentDidMount() {
+    window.title = 'Staycation | Home';
+    window.scrollTo(0, 0);
+  }
+  
   render() {
     return (
       <>
