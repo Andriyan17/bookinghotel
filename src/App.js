3811fccd882e945bdbb58417f81b81897bb641e8
andriyan ginting <andrginting17@gmail.com>
2023-08-01 21:37:10 +0700

    Update router in `App.js`

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'assets/scss/style.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from 'pages/landingPage';
+import DetailPage from 'pages/DetailPage';
 import Example from 'pages/example';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/" component={<LandingPage />}></Route>
+          <Route path="/properties/:id" component={<DetailPage />}></Route>
           <Route path="/example" component={Example}/>
         </Routes>
       </Router>
--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react'
-import Header from 'parts/Header'
-import PageDetailTitle from 'parts/PageDetailsTittle'
+import React, { Component } from 'react';
+import Header from 'parts/Header';
+import PageDetailTitle from 'parts/PageDetailsTittle';
+import ItemDetails from 'json/itemDetails.json'
 
 export default class DetailPage extends Component {
 
     componentDidMount(){
-        window.title = "Staycation | Home";
+        window.title = "Detail Page";
         window.scrollTo(0, 0);
     }
   render() {
+    const breadcrumb = [
+        { pageTitle: "Home", pageHref: "" },
+        { pageTitle: "House Details", pageHref: ""}
+    ]
     return (
-      <div>DetailPage</div>
+      <>
+      <Header {...this.props}></Header>
+      <PageDetailTitle breadcrumb={breadcrumb} data={ItemDetails}></PageDetailTitle>
+      </>
     )
   }
 }
