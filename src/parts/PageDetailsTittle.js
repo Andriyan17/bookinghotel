dbc0eddd4f0c28394a22e9778c0469af03d56f03
andriyan ginting <andrginting17@gmail.com>
2023-08-01 21:22:36 +0700

    Update `PageDetailsTittle.js`

--- src/parts/PageDetailsTittle.js
+++ src/parts/PageDetailsTittle.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import BreadCrumb from 'elements/BreadCrumb';
 
-export default function PageDetailsTittle() {
+export default function PageDetailTitle({ data, breadcrumb }) {
   return (
-    <div>PageDetailsTittle</div>
-  )
+    <section className="container spacing-sm">
+        <div className="row align-items-center">
+          <div className="col">
+            <BreadCrumb data={breadcrumb} />
+          </div>
+          <div className="col-auto text-center">
+            <h1 className="h2">{data.title}</h1>
+            <span className="text-gray-400">
+              {data.city}, {data.country}
+            </span>
+          </div>
+          <div className="col"></div>
+        </div>
+    </section>
+  );
 }
